275da3b9d982bb2a5901c0f05c50dbf83ce1a380
William Nelson <157847943+willsne@users.noreply.github.com>
2024-12-13 13:54:19 -0800

    s

--- singlecycle/regfile.vhd
+++ singlecycle/regfile.vhd
@@ -14,28 +14,33 @@ entity regfile is
         a2 : in std_logic;
         a3 : in std_logic;
         rst_l    : in std_logic;
-      	we : in std_logic;
+      	we3 : in std_logic;
         addr : in std_logic_vector(addrWIDTH - 1 downto 0);
         wd : out std_logic_vector(dataWIDTH - 1 downto 0);
-        rd : out std_logic_vector(dataWIDTH - 1 downto 0);
+        rd1 : out std_logic_vector(dataWIDTH - 1 downto 0);
+        rd2 : out std_logic_vector(dataWIDTH - 1 downto 0);
     );
 end regfile;
 
 architecture Behavioral of regfile is
 
 	type register_array is array (0 to 31) of std_logic_vector(dataWIDTH - 1 downto 0);
-	signal dataRegs : register_array := (others => (others => '0'));
+	signal dataRegsD : register_array := (others => (others => '0'));
+    signal dataRegsQ : register_array := (others => (others => '0'));
+
 
 begin
 
-    RD <= instrRegs(to_integer(unsigned(addr))) when rst_l = '0' else (others => '0')
+    rd1 <= dataRegsQ(to_integer(unsigned(a1)))
+    rd2 <= dataRegsQ(to_integer(unsigned(a2)))
+
 
     process(rst_l, we)
     begin
-        if rst_l = '0' then
-            instrRegs(to_integer(unsigned(addr))) <= (others => (others => '0'));
+        if rst_l = '1' then
+            dataRegsD(to_integer(unsigned(addr))) <= (others => (others => '0'));
         elsif we = '1'
-            instrRegs(to_integer(unsigned(addr))) <= wd;
+            dataRegs(to_integer(unsigned(addr))) <= wd;
         end if;
     end process; 
 
