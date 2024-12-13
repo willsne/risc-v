70277614a710de58fa0c3fa3aa6301016ce8f336
William Nelson <157847943+willsne@users.noreply.github.com>
2024-12-13 14:03:20 -0800

    s

--- singlecycle/regfile.vhd
+++ singlecycle/regfile.vhd
@@ -1,7 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL; -- Use numeric_std for arithmetic
 
 entity regfile is
     generic (
