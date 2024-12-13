c5a0ea36bf58e51373125f5aa5b5e63154e0f491
William Nelson <157847943+willsne@users.noreply.github.com>
2024-12-13 12:31:53 -0800

    add instruction memory

--- singlecycle/instructionmemory.vhd
+++ singlecycle/instructionmemory.vhd
@@ -12,105 +12,27 @@ entity instructionmemory is
         rst_l    : in std_logic;
       	we : in std_logic;
         addr : in std_logic_vector(addrWIDTH - 1 downto 0);
+        wd : out std_logic_vector(dataWIDTH - 1 downto 0);
         rd : out std_logic_vector(dataWIDTH - 1 downto 0);
     );
 end instructionmemory;
 
-architecture Behavioral of SyntaxDemo is
+architecture Behavioral of instructionmemory is
 
 	type register_array is array (0 to 31) of std_logic_vector(dataWIDTH - 1 downto 0);
-	signal registers : register_array := (others => (others => '0'));
+	signal instrRegs : register_array := (others => (others => '0'));
 
+begin
 
-    -- Signal declarations
-    signal internal_signal : std_logic_vector(3 downto 0) := (others => '0');
-    signal temp_signal     : std_logic;
+    RD <= instrRegs(to_integer(unsigned(addr))) when rst_l = '0' else (others => '0')
 
-    -- Type declarations
-    type state_type is (IDLE, WORKING, DONE);
-    signal state : state_type := IDLE;
-
-    -- Constant declarations
-    constant LOCAL_CONSTANT : integer := 10;
-
-    -- Subprogram declarations
-    function add_bits(a, b : std_logic_vector(3 downto 0)) return std_logic_vector is
-        variable result : std_logic_vector(4 downto 0);
+    process(rst_l, we)
     begin
-        result := ('0' & a) + ('0' & b);
-        return result(3 downto 0);
-    end function;
-
-begin 
-registers(to_integer(unsigned(addr))) <= write_data;
-
-    -- Concurrent assignment
-    RD <= internal_signal when state = DONE else (others => '0');
-    
-    -- Process with if-else
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            internal_signal <= (others => '0');
-            state <= IDLE;
-        elsif rising_edge(clk) then
-            case state is
-                when IDLE =>
-                    if input_a = "0000" then
-                        state <= WORKING;
-                    else
-                        state <= IDLE;
-                    end if;
-                when WORKING =>
-                    internal_signal <= add_bits(input_a, input_b);
-                    state <= DONE;
-                when DONE =>
-                    state <= IDLE;
-                when others =>
-                    state <= IDLE;
-            end case;
+        if rst_l = '0' then
+            instrRegs(to_integer(unsigned(addr))) <= (others => (others => '0'));
+        elsif we = '1'
+            instrRegs(to_integer(unsigned(addr))) <= wd;
         end if;
-    end process;
-    
-   
-    -- Process with if-else
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            internal_signal <= (others => '0');
-            state <= IDLE;
-        elsif rising_edge(clk) then
-            case state is
-                when IDLE =>
-                    if input_a = "0000" then
-                        state <= WORKING;
-                    else
-                        state <= IDLE;
-                    end if;
-                when WORKING =>
-                    internal_signal <= add_bits(input_a, input_b);
-                    state <= DONE;
-                when DONE =>
-                    state <= IDLE;
-                when others =>
-                    state <= IDLE;
-            end case;
-        end if;
-    end process;
-
-    -- Concurrent assignment
-    output_z <= internal_signal when state = DONE else (others => '0');
-
-    -- Generate block
-    gen_loop: for i in 0 to 3 generate
-        temp_signal <= input_a(i) and input_b(i);
-    end generate gen_loop;
-
-    -- Block statement
-    MyBlock: block
-        signal block_signal : std_logic_vector(3 downto 0);
-    begin
-        block_signal <= input_a or input_b;
-    end block MyBlock;
+    end process; 
 
 end Behavioral;
